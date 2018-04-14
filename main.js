ce089e18015a69dd56ee1aace6bd29c58dddbe7f
Luke <lukelovemoney@gmail.com>
2018-04-14 16:14:05 +0800

    autoPlay

--- main.js
+++ main.js
@@ -6,5 +6,34 @@ for(let i=0; i<allButtons.length; i++){
         $('#images').css({
             transform: 'translateX(' + p +'px)'
         })
+    n = index
+    allButtons.eq(n%size)
+    .addClass('red')
+    .siblings('.red').removeClass('red')
     })
 }
+
+var n = 0
+var size = allButtons.length
+allButtons.eq(n%size).trigger('click')
+.addClass('red')
+.siblings('.red').removeClass('red')
+
+var timerId = setInterval( () => {
+    n += 1
+    allButtons.eq(n%size).trigger('click')
+    .addClass('red')
+    .siblings('.red').removeClass('red')
+}, 3000)
+
+$('#window').on('mouseenter', function(){
+    window.clearInterval(timerId)
+})
+$('#window').on('mouseleave', function(){
+    timerId = setInterval( () => {
+        n += 1
+        allButtons.eq(n%size).trigger('click')
+        .addClass('red')
+        .siblings('.red').removeClass('red')
+    }, 3000)
+})
